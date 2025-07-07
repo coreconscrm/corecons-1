855c1d12de96b490fde82042ec56cbc7d6848cb1
IntenligenthiA thiA <connection@inteligenthia.com>
2025-07-07 15:43:15 +0000

    Mensaje descriptivo de tu commit inicial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,16 @@ import { getFirestore } from "firebase/firestore";
 // PASO 1: Ve a la configuración de tu proyecto en Firebase.
 // PASO 2: Encuentra la configuración de tu aplicación web (SDK setup and configuration).
 // PASO 3: Copia el objeto de configuración y pégalo aquí para reemplazar este de ejemplo.
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSy...TUS_DATOS_AQUI",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "TUS_DATOS_AQUI",
-  appId: "1:TUS_DATOS_AQUI:web:TUS_DATOS_AQUI"
+  apiKey: "AIzaSyCzeGtW-b-CI-_CPjth_fRXbLE62Smio2A",
+  authDomain: "study-hub-dashboard.firebaseapp.com",
+  projectId: "study-hub-dashboard",
+  storageBucket: "study-hub-dashboard.firebasestorage.app",
+  messagingSenderId: "955037422201",
+  appId: "1:955037422201:web:c7aa3f36793a108029baa8"
 };
+
 
 // --- No es necesario modificar el código debajo de esta línea ---
 
